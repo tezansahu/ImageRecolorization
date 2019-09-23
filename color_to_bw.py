f81156927e846c55cda239ddcb1270e80b05bf20
Tezan <tezansahu@gmail.com>
2019-09-23 23:44:17 +0530

    converted images to b/w

---
+++ color_to_bw.py
@@ -0,0 +1,5 @@
+from PIL import Image
+for i in range(1, 12):
+    img_path = "./color_images/img" + str(i) + ".jpeg"
+    img = Image.open(img_path).convert('LA')
+    img.save("./bw_images/img" + str(i) + ".png")
